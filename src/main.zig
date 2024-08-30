f96009d16436865d21e955941b39d6b4726e3690
Cristian URSU <cristian.ursu.2001@gmail.com>
2024-08-30 14:37:50 -0400

    No more size in hard

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,8 @@
 const std = @import("std");
 
+const screen_width: u16 = 50;
+const screen_height: u16 = 50;
+
 pub fn main() !void {
     const theta_spacing: f16 = 0.07;
     const phi_spacing: f16 = 0.02;
@@ -7,7 +10,7 @@ pub fn main() !void {
     const r1 = 1;
     const r2 = 2;
     const k2 = 5;
-    const k1: u32 = 60 * k2 * 3 / (8 * (r1 + r2)); // 60 == screen_width
+    const k1: u32 = screen_width * k2 * 3 / (8 * (r1 + r2));
 
     var A: f64 = 1.0;
     var B: f64 = 1.0;
@@ -21,11 +24,11 @@ pub fn main() !void {
         A += 0.007;
         B += 0.003;
 
-        var output: [60][60]u8 = undefined;
+        var output: [screen_width][screen_height]u8 = undefined;
         for (&output) |*row| {
-            row.* = [_]u8{' '} ** 60;
+            row.* = [_]u8{' '} ** screen_height;
         }
-        const zbuffer = std.mem.zeroes([60][60]u8);
+        const zbuffer = std.mem.zeroes([screen_width][screen_height]u8);
 
         const end: f64 = 2 * std.math.pi;
         var theta: f64 = 0.0;
@@ -45,8 +48,8 @@ pub fn main() !void {
                 const y = circlex * (sinB * cos_phi - sinA * cosB * sin_phi) + circley * cosA * cosB;
                 const ooz: f64 = 1 / (k2 + cosA * circlex * sin_phi + sinA * circley);
 
-                const xp: u64 = @intFromFloat(60 / 2 + k1 * ooz * x);
-                const yp: u64 = @intFromFloat(60 / 2 - k1 * ooz * y);
+                const xp: u64 = @intFromFloat(screen_width / 2 + k1 * ooz * x);
+                const yp: u64 = @intFromFloat(screen_height / 2 - k1 * ooz * y);
 
                 const L = cos_phi * cos_theta * sinB - cosA * cos_theta * sin_phi - sinA * sin_theta + cosB * (cosA * sin_theta - cos_theta * sinA * sin_phi);
 
@@ -62,8 +65,8 @@ pub fn main() !void {
         }
 
         std.debug.print("\x1b[H", .{});
-        for (0..60) |j| {
-            for (0..60) |i| {
+        for (0..screen_height) |j| {
+            for (0..screen_width) |i| {
                 std.debug.print("{c}{c}", .{ output[i][j], output[i][j] });
             }
             std.debug.print("\n", .{});
